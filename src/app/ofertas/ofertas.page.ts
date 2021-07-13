1d580aab266fda798a048ffae9150584c9876d13
Nathan <nathanrambalducci@edu.unifil.br>
2021-07-13 00:02:10 -0300

    A BARRA DE OFERTAS ESTÁ FEITA
    A PORRA APARECE AGORA CARALHO! TUDO BUNITINHO DE 3 POR 3!
    FIQUEI A NOITE TODA NISSO!!!!!!!!!!
    PRAISE THE SUN!!!!!!!!!!!!!!!!!!!!

--- src/app/ofertas/ofertas.page.ts
+++ src/app/ofertas/ofertas.page.ts
@@ -10,24 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OfertasPage implements OnInit {
 
-  constructor(private http:HttpClient) {  }
+  constructor(private http:HttpClient) {
+    this.mostrarLista(); 
+    this.getLista();
+   }
 
   private minhalista;
 
   public getLista() {
-    return this.minhalista
+    return this.minhalista;
   }
 
-  mostrarLista() { // pegar a database ofertas e os diagrams que queremos colocar
+ mostrarLista() { // pegar a database ofertas e os diagrams que queremos colocar
     this.minhalista=[];
-    this.http.get<any[]>("http://localhost/api/ConsultaOfertas.php").subscribe ( dados => { dados.forEach( item => {
-      this.minhalista([item.NumAnun,item.NomeAnun,item.PreAnun])
+    this.http.get<any[]>("http://localhost/api/ofertas/ConsultaNumOfertas.php").subscribe 
+    ( dados => { dados.forEach( item => {
+      this.minhalista.push([item.NumAnun, item.NomeAnun, item.PreAnun]) 
+      console.log(item.NumAnun)
     })
   })
 
   }
 
   ngOnInit() {
+    this.mostrarLista;
   }
 
 }
